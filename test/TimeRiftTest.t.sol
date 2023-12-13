78512e3a750c02402404504ca4adccec0e7dcc89
defichad23 <alex@possumfinance.com>
2023-12-13 20:41:57 +0100

    Initial commit

---
+++ test/TimeRiftTest.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {TimeRift} from "../src/TimeRift.sol";
+
+contract TimeRiftTest is Test {}
